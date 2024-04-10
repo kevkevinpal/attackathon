c58db1fd0f8ee4ff7777d37123ab5aa1a3c8287e
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-04-10 15:52:22 -0400

    scripts: fix docker detection in warnet stop

--- scripts/stop_warnet.sh
+++ scripts/stop_warnet.sh
@@ -7,6 +7,8 @@ fi
 
 cd warnet
 
+docker_info=$(docker info)
+
 # Setup depends on docker or docker desktop.
 if grep -q "Context:.*desktop" <<< "$docker_info"; then
     echo "Stopping warnet for docker desktop."
